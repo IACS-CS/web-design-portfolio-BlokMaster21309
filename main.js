cb05e3ad18e1c2e403d35801f12d7d85ccdcb696
BlokMaster21309 <bmaster42070@gmail.com>
2026-01-13 17:42:44 -0500

    added animations to some things

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("this message is all i do!");
+// Get references to the button and the target element
+const scrollButton = document.getElementById('arrdown');
+const targetSection = document.getElementById('content');
+
+// Add an event listener to the button
+scrollButton.addEventListener('click', () => {
+  // Use scrollIntoView with smooth behavior options
+  targetSection.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start' // Aligns the top of the element to the top of the viewport
+  });
+});
